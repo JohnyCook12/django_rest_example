60f7263adf6086c25600f0194f159eacdffb5465
john.yy <john.yy@seznam.cz>
2022-12-08 15:40:35 +0100

    tests done

--- django_rest_example/grouping/tests.py
+++ django_rest_example/grouping/tests.py
@@ -1,28 +1,22 @@
+
 from urllib import parse
-
-from django.test import TestCase, Client
-
 import json, requests
-from django.contrib.auth.models import User
-from django.urls import reverse
 from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.test import APITestCase, APIRequestFactory
-from django.urls import reverse
-
-from .serializers import PersonSerializer, PersonCreateSerializer, GroupSerializer
-from .models import Group, Person
+from rest_framework.test import APITestCase
 
 
 class PersonTests(APITestCase):
+    """ Test class for Grouping API app on http://127.0.0.1:8000/grouping/ """
+
     base_url = 'http://127.0.0.1:8000/grouping/'
 
-    def test_create_person(self, name="John Doe", age=40):
+    def create_person(self, name, age):
         """
-        Creates person by POST to /people/
+        Creates person by POST to /people/ and returns it´s ID.
+        Raises AssertionError if server does not respond with HTTP 201 or names doesn´t match.
         :param name:
         :param age:
-        :return:
+        :return: id:
         """
 
         headers = {'accept': 'application/json',
@@ -31,17 +25,18 @@ class PersonTests(APITestCase):
             parse.urljoin(self.base_url, 'people'),
             data=json.dumps({'name': name, 'age': age}),
             headers=headers)
-
-        print('vytvoreno ID: ', person_response.json()['id'])
-
         self.assertEqual(person_response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(person_response.json()['name'], "John Doe")
+        self.assertEqual(person_response.json()['name'], name)
+        self.assertEqual(person_response.json()['age'], age)
+        return person_response.json()['id']
 
-    def test_create_group(self, name="football"):
+
+    def create_group(self, name):
         """
-        Creates group by POST to /groups/
+        Creates group by POST to /groups.
+        Raises AssertionError if server does not respond with HTTP 201 or names doesn´t match.
         :param name:
-        :return:
+        :return id of group:
         """
         headers = {'accept': 'application/json',
                    'content-type': 'application/json'}
@@ -53,12 +48,14 @@ class PersonTests(APITestCase):
         print('created group ID: ', group_response.json()['id'])
 
         self.assertEqual(group_response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(group_response.json()['name'], "football")
+        self.assertEqual(group_response.json()['name'], name)
+        return group_response.json()['id']
 
-    def test_assert_person_exists(self, person_id=26):
+
+    def retrieve_existing_person(self, person_id):
         """
-        Tries to retrieve person from /people/{id}. Raises AssertionError
-        if server does not respond with HTTP 200.
+        Tries to retrieve person from /people/{id}.
+        Raises AssertionError if server does not respond with HTTP 200.
         :param person_id:
         :return:
         """
@@ -66,187 +63,126 @@ class PersonTests(APITestCase):
             parse.urljoin(self.base_url, f'people/{person_id}'),
             headers={'accept': 'application/json'})
 
-        print('zaznam ID=26: ',person_response.json())
         assert person_response.status_code == 200, \
             f"Get person didn't return expected status code, " \
             f"it returned: {person_response.status_code}"
 
-    def test_assert_all_existing_persons(self):
+
+    def retrieve_existing_group(self, group_id):
         """
-        Tries to retrieve person from /people/{id}. Raises AssertionError
-        if server does not respond with HTTP 200.
-        :param person_id:
+        Tries to retrieve group from /people/{id}.
+        Raises AssertionError if server does not respond with HTTP 200.
+        :param group_id:
         :return:
+        """
+        group_response = requests.get(
+            parse.urljoin(self.base_url, f'groups/{group_id}'),
+            headers={'accept': 'application/json'})
+
+        assert group_response.status_code == 200, \
+            f"Get person didn't return expected status code, " \
+            f"it returned: {group_response.status_code}"
+
+
+    def assign_person_to_group(self, person_id, group_id):
+        """
+        Tries to assign person from /people/{id} to group. Raises AssertionError
+        if server does not respond with HTTP 200 or names doesn´t match.
+        :param person_id:
+        :param group_id:
+        :return:
+        """
+        headers = {'accept': 'application/json',
+                   'content-type': 'application/json'}
+        person_response = requests.patch(
+            parse.urljoin(self.base_url, f'people/{person_id}'),
+            data=json.dumps({'groups': [group_id]}),
+            headers=headers)
+
+        self.assertEqual(person_response.status_code, status.HTTP_200_OK)
+        self.assertEqual(person_response.json()['groups'], [group_id])
+
+
+    def count_all_persons(self):
+        """
+        Tries to retrieve persons from /people and count results.
+        Raises AssertionError if server does not respond with HTTP 200.
+        :return results count:
         """
         person_response = requests.get(
             parse.urljoin(self.base_url, 'people'),
             headers={'accept': 'application/json'})
 
-        print('vsecky ID: ',len(person_response.json()))
         assert person_response.status_code == 200, \
             f"Get person didn't return expected status code, " \
             f"it returned: {person_response.status_code}"
+        return len(person_response.json())
 
 
-# =============================================================================================================================
-# =============================================================================================================================
-# =============================================================================================================================
+    def delete_group(self, group_id):
+        """
+        Delete group by ID.
+        Raises AssertionError if server does not respond with HTTP 204.
+        :param group_id:
+        :return:
+        """
+        headers = {'accept': 'application/json',
+                   'content-type': 'application/json'}
+        group_response = requests.delete(
+            parse.urljoin(self.base_url, f'groups/{group_id}'),
+            data=json.dumps({'id': group_id}),
+            headers=headers)
+
+        print('Deleted group ID: ', group_id)
+        self.assertEqual(group_response.status_code, status.HTTP_204_NO_CONTENT)
 
 
-# class RegistrationTestCase(APITestCase):
-#     def test_registration(self):
-#         data = {"username": "Peter", "email": "john.yyy@seznam.cz", "password1": "Weakpass123", "password2": "Weakpass123"}
-#         response = self.client.post("admin/registration", data)
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    def delete_person(self, person_id):
+        """
+        Delete group by ID.
+        Raises AssertionError if server does not respond with HTTP 204.
+        :param person_id:
+        :return:
+        """
+        headers = {'accept': 'application/json',
+                   'content-type': 'application/json'}
+        group_response = requests.delete(
+            parse.urljoin(self.base_url, f'people/{person_id}'),
+            data=json.dumps({'id': person_id}),
+            headers=headers)
+
+        print('Deleted person ID: ', person_id)
+        self.assertEqual(group_response.status_code, status.HTTP_204_NO_CONTENT)
 
 
-# class TestClient:
-#     def __init__(self, base_url):
-#         self.base_url = base_url
-#
-#     def assert_person_exists(self, person_id):
-#         """
-#         Tries to retrieve person from /people/{id}. Raises AssertionError
-#         if server does not respond with HTTP 200.
-#         :param person_id:
-#         :return:
-#         """
-#         person_response = requests.get(
-#             parse.urljoin(self.base_url, f'people/{person_id}'),
-#             headers={'accept': 'application/json'})
-#         assert person_response.status_code == 200, \
-#             f"Get person didn't return expected status code, " \
-#             f"it returned: {person_response.status_code}"
-#
-#     def create_person(self, name="John Doe", age=40):
-#         """
-#         Creates person by POST to /people/
-#         :param name:
-#         :param age:
-#         :return:
-#         """
-#         headers = {'accept': 'application/json',
-#                    'content-type': 'application/json'}
-#         person_response = requests.post(
-#             parse.urljoin(self.base_url, 'people/'),
-#             data=json.dumps({'name': name, 'age': age}),
-#             headers=headers)
-#
-#         assert person_response.status_code == 201, \
-#             f"Create person didn't return expected status code, it returned: " \
-#             f"{person_response.status_code}"
-#
-#         return person_response.json()
-#
-#
-# class GroupingAPITestCase(APITestCase):
-#
-#     def test_created_person_can_be_retrieved(self):
-#         # client = TestClient(f'http://localhost:8000')
-#         client = TestClient(f'http://127.0.0.1:8000/grouping')
-#         person = client.create_person(name="John Doe", age=40)
-#         client.assert_person_exists(person['id'])
+    def test_all(self):
+        """
+        Testing:
+        Get person info and count.
+        Create person and group.
+        Retrieve created person.
+        Check if person count raised by 1.
+        Delete created person and group
+        Check if person count decreased by 1.
+        :return:
+        """
 
+        person_count_before_creating = self.count_all_persons()
 
+        test_person_id = self.create_person('John Doe', 40)
+        test_group_id = self.create_group('hockey_team')
 
-# =====================   post fungovalo, ale get nikdy nic nenašlo. DB se vždy smazala.   ======================
-#
-# class GroupingAPITestCase(APITestCase):
-#
-#     base_url = 'http://127.0.0.1:8000/grouping/'
-#
-#     def test_create_person(self):
-#         previous_person_count = Person.objects.all().count()
-#         sample_person = {"name": "Peter", "age": 20}
-#         response = self.client.post(reverse('person_list'), sample_person)
-#
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Person.objects.all().count(), previous_person_count+1)
-#         self.assertEqual(response.data["name"], "Peter")
-#         print("created id: ",response.data)
-#
-#     def test_create_group(self):
-#         previous_group_count = Group.objects.all().count()
-#         sample_group = {"name": "football"}
-#         response = self.client.post(reverse('group_list'), sample_group)
-#
-#         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-#         self.assertEqual(Group.objects.all().count(), previous_group_count+1)
-#         self.assertEqual(response.data["name"], "football")
-#         self.assertEqual(response.data["id"], 1)
-#
-#
-#
-#     def test_person_exists2(self, person_id=1):
-#         """
-#         Tries to retrieve person from /people/{id}. Raises AssertionError
-#         if server does not respond with HTTP 200.
-#         :param person_id:
-#         :return:
-#         """
-#
-#         response = Client().get(reverse('person_list'))
-#         # response = self.client.get(reverse('person_list'))
-#         # response = self.client.get(reverse('person'), sample_person)
-#         print("RESPO: ", response.status_code)
-#         print("RESPO: ", response.content)
-#
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-#         # self.assertEqual(response.data['name'], 'Peter')
+        self.retrieve_existing_person(test_person_id)
+        self.retrieve_existing_group(test_group_id)
+        self.assign_person_to_group(test_person_id, test_group_id)
 
+        person_count_after_creating = self.count_all_persons()
 
-# ===========================================
+        assert person_count_after_creating == person_count_before_creating + 1, \
+            "person count didn´t increase by 1 after creating new person"
+        self.delete_group(test_group_id)
+        self.delete_person(test_person_id)
+        person_count_after_deleting = self.count_all_persons()
 
-    #
-    # def test_create_person(self, name="John Doe", age=40):
-    #     """
-    #     Creates person by POST to /people/
-    #     :param name:
-    #     :param age:
-    #     :return:
-    #     """
-    #     headers = {'accept': 'application/json',
-    #                'content-type': 'application/json'}
-    #     person_response = requests.post(
-    #         parse.urljoin(self.base_url, 'people'),
-    #         data=json.dumps({'name': name, 'age': age}),
-    #         headers=headers)
-    #
-    #     self.assertEqual(person_response.status_code, status.HTTP_201_CREATED)
-
-    # def test_assert_person_exists(self, person_id=1):
-    #     """
-    #     Tries to retrieve person from /people/{id}. Raises AssertionError
-    #     if server does not respond with HTTP 200.
-    #     :param person_id:
-    #     :return:
-    #     """
-    #     person_response = requests.get(
-    #         parse.urljoin(self.base_url, f'people/{person_id}'),
-    #         headers={'accept': 'application/json'})
-    #     assert person_response.status_code == 200, \
-    #         f"Get person didn't return expected status code, " \
-    #         f"it returned: {person_response.status_code}"
-
-    # def test_asign_to_group(self):
-    #
-    #     sample_person = {"id":1, "age": 22, "group": 1}
-    #     response = self.client.patch(reverse('person'), sample_person)
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     # self.assertEqual(Group.objects.all().count(), previous_group_count + 1)
-    #     self.assertEqual(response.data["group"], 1)
-    #     self.assertEqual(response.data["age"], 22)
-
-    # def test_person_exists(self):
-    #
-    #     response = self.client.get(reverse('person_list'))
-    #
-    #     # self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     # self.assertEqual(Person.objects.all().count(), previous_person_count+1)
-    #     # self.assertEqual(response.data["name"], "Peter")
-    #     print("response: ",response)
-    #     print("response: ",response.json())
-
-
-
+        assert person_count_after_creating == person_count_after_deleting + 1, \
+            "person count didn´t decrease by 1 after deleting person"
